1203b3c4fdd4db2af976ab0b53ce3fc86e255e87
Zerun <tian.ze@husky.neu.edu>
2019-02-13 18:32:31 -0500

    add service answers look

---
+++ src/components/ServiceAnswers.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+class ServiceAnswers extends React.Component {
+    render() {
+        return(
+            <div>
+                <h3>Service Answers</h3>
+                <table className="table">
+                    <tbody>
+                    {
+
+                    }
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
+}
+
+export default ServiceAnswers
