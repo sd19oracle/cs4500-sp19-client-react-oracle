c99d51924e62cfe3b3607a3333af58259066b2c6
cvanzy <cao.zhi@husky.neu.edu>
2019-02-13 23:20:58 -0500

    finish service question middle tire

--- src/components/ServiceQuestions.js
+++ src/components/ServiceQuestions.js
@@ -1,23 +1,43 @@
 import React from 'react'
-
+import ServiceQuestionService from '../services/ServiceQuestionService'
 class ServiceQuestions extends React.Component {
+    constructor(props) {
+        super(props)
+        this.serviceQuestionService = ServiceQuestionService.getInstance()
+        this.state = {
+            serviceQuestions: []
+        }
+    }
+    componentDidMount() {
+        this.serviceQuestionService
+            .findAllServiceQuestions()
+            .then(serviceQuestions =>
+                this.setState({
+                    serviceQuestions: serviceQuestions
+                })
+            )
+    }
+
+    myFunction() {
+        console.log(typeof(window.location.href));
+    }
+
     render() {
-        return (
+        return(
             <div>
                 <h3>Service Questions</h3>
-                <table className="table">
-                    {
-                    /* <tbody>
+                <button onClick={this.myFunction}>Try it</button>
+                <table className="table">    
+                    <tbody>
                     {
                         this.state.serviceQuestions
                             .map(serviceQuestion =>
                                 <tr key={serviceQuestion.id}>
-                                    <td>{serviceQuestion.question}</td>
+                                    <td>{serviceQuestion.title}</td>
                                 </tr>
                             )
                     }
-                    </tbody> */}
-                <h1>Service Questions</h1>
+                    </tbody>
                 </table>
             </div>
         )
