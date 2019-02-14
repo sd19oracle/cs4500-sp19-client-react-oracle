b316f2e2ad00f46e50e8c2604abe7b6fda99d43c
Zerun <tian.ze@husky.neu.edu>
2019-02-13 23:24:27 -0500

    dynamically change url

--- src/services/ServiceAnswerService.js
+++ src/services/ServiceAnswerService.js
@@ -1,18 +1,28 @@
 class ServiceAnswerService {
     static instance = null;
+    static hostname = null
+    static urlPrefix = null
     static getInstance() {
         if (ServiceAnswerService.instance === null) {
             ServiceAnswerService.instance = new ServiceAnswerService()
         }
+        this.hostname = window.location.hostname
+        if (this.hostname === "localhost") {
+            this.urlPrefix = 'http://localhost:8080/'
+        } else {
+            this.urlPrefix = 'https://cs4500-sp19-oracle.herokuapp.com/'
+        }
+        console.log(this.hostname)
+        console.log(this.urlPrefix)
         return this.instance
     }
 
     findAllServiceSpecificAnswers = () =>
-        fetch('http://localhost:8080/api/servicesSpecificAnswers')
+        fetch(ServiceAnswerService.urlPrefix + 'api/servicesSpecificAnswers')
             .then(response => response.json())
 
     findOneAnswer = id =>
-        fetch('http://localhost:8080/api/servicesSpecificAnswers/{answerID}')
+        fetch(ServiceAnswerService.urlPrefix + 'api/servicesSpecificAnswers/{id}')
             .then(response => response.json())
 }
 
