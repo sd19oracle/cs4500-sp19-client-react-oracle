5f04d42c205fb996c39487a978b993b3b9cbbb2a
Jiahao Cai <cai.jiah@husky.neu.edu>
2019-02-15 13:31:26 -0500

    add the render page

--- src/components/ServiceQuestionDetails.js
+++ src/components/ServiceQuestionDetails.js
@@ -17,12 +17,12 @@ class ServiceQuestionDetails extends React.Component {
         this.serviceQuestionService
             .findAllServiceQuestions()
             .then(serviceQuestions => {
-                    this.props.history.push("/admin/service-questions/" + serviceQuestions[0].id)
-                    this.setState({
-                        serviceQuestions: serviceQuestions,
-                        serviceQuestion: serviceQuestions[0]
-                    })
-                }
+                this.props.history.push("/admin/service-questions/" + serviceQuestions[0].id)
+                this.setState({
+                    serviceQuestions: serviceQuestions,
+                    serviceQuestion: serviceQuestions[0]
+                })
+            }
             )
     }
 
@@ -30,12 +30,40 @@ class ServiceQuestionDetails extends React.Component {
         this.serviceQuestionService
             .findServiceQuestionById(id)
             .then(serviceQuestion => {
-                    this.props.history.push("/admin/service-questions/" + id)
-                    this.setState({
-                        serviceQuestion: serviceQuestion
-                    })
-                }
+                this.props.history.push("/admin/service-questions/" + id)
+                this.setState({
+                    serviceQuestion: serviceQuestion
+                })
+            }
             )
+    render() {
+        return (
+            <div>
+                <h3>Service Question Details</h3>
+                <select
+                    value={this.state.serviceQuestion.id}
+                    onChange={(e) => this.selectServiceQuestion(e.target.value)}
+                    className="form-control">
+                    {
+                        this.state.serviceQuestions
+                            .map(serviceQuestion =>
+                                <option
+                                    value={serviceQuestion.id}
+                                    key={serviceQuestion.id}>
+                                    {serviceQuestion.question}
+                                </option>
+                            )
+                    }
+                </select>
+                <label>Service Question Question</label><br />
+                <input
+                    onChange={() => { }}
+                    className="form-control"
+                    value={this.state.serviceQuestion.question} />
+            </div>
+        )
+    }
+
 }
 
 export default ServiceQuestionDetails
