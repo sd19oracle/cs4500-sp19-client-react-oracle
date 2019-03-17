d8bc2831d9609ff7420a345e1194c8e2851284c9
Jacob Hutchinson <hutchinson.j@husky.neu.edu>
2019-03-16 21:10:40 -0400

    Added functionality to access api layer rather than just localhost for FAQ Answer Details screen

--- src/services/FAQAnswerService.js
+++ src/services/FAQAnswerService.js
@@ -1,15 +1,28 @@
 export default class FAQAnswerService {
-    static instance = null;
-    static getInstance() {
-        if(FAQAnswerService.instance === null) {
-            FAQAnswerService.instance = new FAQAnswerService()
-        }
-        return this.instance
-    }
-    findFAQAnswerById = id =>
-        fetch(`http://localhost:8080/api/faq-answers/${id}`)
-            .then(response => response.json());
-    findAllFAQAnswers = () =>
-        fetch("http://localhost:8080/api/faq-answers")
-            .then(response => response.json());
+	static instance = null;
+	static hostname = null;
+	static urlprefix = null;
+
+	static getInstance() {
+		if(FAQAnswerService.instance === null) {
+			FAQAnswerService.instance = new FAQAnswerService()
+		}
+		this.hostname = window.location.hostname
+		if(this.hostname === "localhost") {
+			this.urlprefix = 'http://localhost:8080/'
+		} else {
+			this.urlprefix = 'https://cs4500-sp19-oracle.herokuapp.com/'
+		}
+		console.log(this.hostname)
+		console.log(this.urlprefix)
+
+		return this.instance
+
+	}
+	findFAQAnswerById = id =>
+		fetch(FAQAnswerService.urlprefix + `api/faq-answers/${id}`)
+			.then(response => response.json());
+	findAllFAQAnswers = () =>
+		fetch(FAQAnswerService.urlprefix + "api/faq-answers")
+			.then(response => response.json());
 }
