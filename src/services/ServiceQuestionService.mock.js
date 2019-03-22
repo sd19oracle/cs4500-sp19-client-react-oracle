93e96aa57d432dd46d917fc1c2fa317a010fdcc1
Zerun <tian.ze@husky.neu.edu>
2019-03-22 14:15:33 -0400

    add mock filter service test

--- src/services/ServiceQuestionService.mock.js
+++ src/services/ServiceQuestionService.mock.js
@@ -4,7 +4,6 @@ global.fetch = jest.fn()
     .mockImplementation((url, config) => {
         console.log("Run mock service");
         if (!config) {
-
             if (url.indexOf('api/servicesSpecificQuestions/0') != -1) {
                 return new Promise((resolve, reject) => {
                     resolve({
@@ -24,7 +23,27 @@ global.fetch = jest.fn()
             }
 
         } else if (config.method === 'post') {
-
+            let serviceQuestion = JSON.parse(config.body)
+            if (url.includes('api/servicesSpecificQuestions/filter')) {
+                // this filtering feature is implemented in the backend, it would
+                // be reasonable to assume the backend has implemented it correctly.
+                // The mock service here won't bother to make filtering fully functional
+                // but return a smaller list to validate if the frontend is updated.
+                let filterTitle = serviceQuestion.title;
+                let filteredQuestions = [];
+                for (let i = 0; i < serviceQuestions.length; i++) {
+                    if (serviceQuestions[i].title.toLowerCase().indexOf(filterTitle.toLowerCase()) >= 0) {
+                        filteredQuestions.push(serviceQuestions[i])
+                    }
+                }
+                return new Promise((resolve, reject) => {
+                    resolve({
+                        json: function () {
+                            return filteredQuestions
+                        }
+                    })
+                })
+            }
         } else if (config.method === 'put') {
 
         } else if (config.method === 'delete') {
