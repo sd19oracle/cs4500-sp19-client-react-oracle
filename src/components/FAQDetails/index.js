1a6ec44b7c66b503f144b0fb853c60cfe283a197
Jacob Hutchinson <jacobhutchinson@Jacobs-MacBook-Pro.local>
2019-04-12 15:17:29 -0400

    Fixed bug where wrong file had been duplicated

--- src/components/FAQDetails/index.js
+++ src/components/FAQDetails/index.js
@@ -1,108 +1,54 @@
 import React from "react";
-import FAQAnswerService from "../../services/FAQAnswerService";
+import FAQService from "../../services/FAQService";
 import "./index.css";
-import * as qs from "query-string";
 
-export default class FAQAnswerDetails extends React.Component {
+export default class FAQDetails extends React.Component {
   constructor(props) {
     super(props);
-    this.faqAnswerService = FAQAnswerService.getInstance();
-    this.state = {
-      faqAnswer: {
-        answer: "",
-        id: 1,
-        questionId: 1,
-        question: ""
-      },
-      isEditing: false
-    };
-    this.enableEditing = this.enableEditing.bind(this);
-    this.save = this.save.bind(this);
-    this.handleInput = this.handleInput.bind(this);
-    this.deleteAnswer = this.deleteAnswer.bind(this);
-    this.cancelEditing = this.cancelEditing.bind(this);
+    this.faqService = FAQService.getInstance();
+    this.state = {faq: {id: 1}};
+    this.addAnswer = this.addAnswer.bind(this);
+
   }
 
   componentDidMount() {
     this.load(this.props.match.params.id);
-    qs.parse(this.props.location.search).edit && this.enableEditing();
   }
 
   load(id) {
-    this.faqAnswerService
-      .findFAQAnswerById(id)
-      .then(answer => this.setState({
-        faqAnswer: answer
-      }));
+    this.faqService
+      .findFAQById(id)
+      .then(faq => this.setState({faq}));
   }
 
-  enableEditing() {
-    this.setState({isEditing: true});
-  }
-
-  cancelEditing() {
-    this.setState({isEditing: false});
-    this.load(this.props.match.params.id);
-  }
-
-  save() {
-    this.setState({isEditing: false});
-    this.faqAnswerService.updateFAQAnswer(this.state.faqAnswer);
-  }
-
-  deleteAnswer() {
-    window.confirm("Are you sure you want to delete this FAQ Answer?")
-    && this.faqAnswerService.deleteFAQAnswerById(this.state.faqAnswer.id)
-      .then(() => this.props.history.push("/admin/faq-answers"))
-  }
-
-  handleInput(e) {
-    this.setState({faqAnswer: {...this.state.faqAnswer, answer: e.currentTarget.value}});
+  addAnswer() {
+    this.faqService.addAnswer(this.props.match.params.id)
+      .then(answer => this.props.history.push(`/admin/faq-answers/${answer.id}?edit=true`));
   }
 
   render() {
     return (
       <div className={"grid-container"}>
         <div className="d-flex">
-          <h3 className={"mr-1"}>FAQ Answer Details</h3>
-          {this.state.isEditing || <button className={"btn btn-warning"} onClick={this.enableEditing}>Edit</button>}
-          {this.state.isEditing && <button className={"btn btn-secondary"} onClick={this.cancelEditing}>Cancel</button>}
-          {this.state.isEditing && <button className={"btn btn-success"} onClick={this.save}>Save</button>}
-          <button className={"btn btn-danger"} onClick={this.deleteAnswer}>Delete</button>
-        </div>
-        <div className={"d-flex"}>
-          <label className={"flex-shrink-0 m-1"} htmlFor={"id"}>Answer ID</label>
-          <input
-            id={"id"}
-            readOnly={true}
-            value={this.state.faqAnswer.id}
-            className="form-control" />
+          <h3>FAQ Details</h3>
+          <button className={"btn btn-primary"} onClick={this.addAnswer}>Add Answer</button>
         </div>
         <div className={"d-flex"}>
           <label className={"flex-shrink-0 m-1"} htmlFor={"questionId"}>Question ID</label>
           <input
             id={"questionId"}
             readOnly={true}
-            value={this.state.faqAnswer.questionId}
+            value={this.state.faq.id}
             className="form-control">
           </input>
         </div>
         <div className={"d-flex"}>
-          <label className={"flex-shrink-0 m-1"} htmlFor={"question"}>FAQ Answer Question</label>
+          <label className={"flex-shrink-0 m-1"} htmlFor={"question"}>Question</label>
           <input
             id={"question"}
             readOnly={true}
             className="form-control"
-            value={this.state.faqAnswer.question}/>
-        </div>
-        <div className={"d-flex"}>
-          <label className={"flex-shrink-0 m-1"} htmlFor={"answer"}>FAQ Answer Answer</label>
-          <input
-            id={"answer"}
-            readOnly={!this.state.isEditing}
-            className={"form-control"}
-            value={this.state.faqAnswer.answer}
-            onChange={this.handleInput}/>
+            value={this.state.faq.question}/>
         </div>
       </div>
     )
