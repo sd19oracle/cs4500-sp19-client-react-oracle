ea66041222b45d9248248cb7dbbb2411d0e87fed
cvanzy <cao.zhi@husky.neu.edu>
2019-04-19 00:46:50 -0400

    Add a dropdown selection to logout and see profile for login user

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import {BrowserRouter as Router, Route, Link, Redirect} from 'react-router-dom'
 import Admin from './components/Admin'
 import Home from './components/Home'
+import Profile from './components/Profile'
 import ServiceService from './services/ServiceService'
 import SignUp from './components/SignUp'
 import {GiWyvern} from "react-icons/gi";
 import popularCategories from './data/popular-service-categories.mock'
+import Dropdown from 'react-bootstrap/Dropdown'
+import DropdownButton from 'react-bootstrap/DropdownButton'
 
 export default class App extends Component {
     constructor(props) {
@@ -17,11 +20,16 @@ export default class App extends Component {
             username: "Jose"
         };
         this.logout = this.logout.bind(this);
+        this.goToProfile = this.goToProfile.bind(this);
     }
 
     logout = () => {
         this.setState({username: ""});
     };
+
+    goToProfile = () => {
+        this.props.history.push('/profile')
+    }
 
     componentDidMount() {
         for (let i in popularCategories) {
@@ -57,8 +65,14 @@ export default class App extends Component {
                         {this.state.username !== "" ?
 
                             (<div className="text-right">
+                                <DropdownButton id="dropdown-item-button" title={this.state.username}>
+                                    <Dropdown.Item href="/profile">Profile</Dropdown.Item>
+                                    <Dropdown.Item as="button" onClick={this.logout}>Log out</Dropdown.Item>
+                                </DropdownButton>;
                                 {"Welcome  " + this.state.username}
+                                <Link to="/profile"> Profile </Link>
                                 <button className="button" onClick={this.logout}>Log Out</button>
+                                
                             </div>) : (
                                 <div>
                                     <div className="text-right">
@@ -89,6 +103,9 @@ export default class App extends Component {
                         <Route
                             path="/admin"
                             component={Admin}/>
+                        <Route
+                            path="/profile"
+                            component={Profile} />
                     </div>
                 </Router>
             </div>
---
+++ src/components/Profile.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import ServiceAnswerService from '../services/ServiceAnswerService'
+class Profile extends React.Component {
+    constructor(props) {
+        super(props)
+        this.serviceAnswerService = ServiceAnswerService.getInstance()
+        this.state = {
+            serviceAnswers: [],
+            serviceAnswer: {
+                id: 1,
+                answer: ''
+
+            }
+        }
+    }
+    componentDidMount() {
+        this.serviceAnswerService
+            .findAllServiceSpecificAnswers()
+            .then(serviceAnswers => {
+                    this.props.history.push("/admin/service-answers/" + serviceAnswers[0].id)
+                    this.setState({
+                        serviceAnswers: serviceAnswers,
+                        serviceAnswer: serviceAnswers[0]
+                    })
+                }
+            )
+    }
+    selectServiceAnswer = id =>
+        this.serviceAnswerService
+            .findOneAnswer(id)
+            .then(serviceAnswer => {
+                    this.props.history.push("/admin/service-answers/" + id)
+                    this.setState({
+                        serviceAnswer: serviceAnswer
+                    })
+                }
+            )
+    render() {
+        return(
+            <div>
+                <h3>Service Answer Details</h3>
+                <select
+                    value={this.state.serviceAnswer.id}
+                    onChange={e => {this.selectServiceAnswer(e.target.value)}}
+                    className="form-control">
+                    {
+                        this.state.serviceAnswers
+                            .map(serviceAnswer =>
+                                <option
+                                    value={serviceAnswer.id}
+                                    key={serviceAnswer.id}>
+                                    {serviceAnswer.id}) {serviceAnswer.answer}
+                                </option>
+                            )
+                    }
+                </select>
+                <label>Service Answer Answer</label><br/>
+                <input
+                    onChange={() => {}}
+                    className="form-control"
+                    value={this.state.serviceAnswer.answer}/>
+            </div>
+        )
+    }
+}
+
+export default Profile
