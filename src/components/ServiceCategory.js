63f93b4cc328a86c8091e2e5b527d940594448f1
Nianmin Guo <zenmai0822@live.com>
2019-04-20 18:48:27 -0400

    Added Admin ServiceCategory Page

--- src/components/ServiceCategory.js
+++ src/components/ServiceCategory.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ServiceCategoryService from '../services/ServiceCategoryService'
+import { MdAdd, MdSave, MdEdit, MdDelete } from "react-icons/md"
 class ServiceCategories extends React.Component {
     constructor(props) {
         super(props)
@@ -12,29 +13,64 @@ class ServiceCategories extends React.Component {
         this.serviceCategoryService
             .findAllServiceCategories()
             .then(serviceCategories =>
-        this.setState({
-            serviceCategories: serviceCategories
-        })
-    )
+                this.setState({
+                    serviceCategories: serviceCategories
+                })
+            )
     }
     render() {
-        return(
+        return (
             <div>
-            <h3>Service Categories</h3>
-        <table className="table">
-            <tbody>
-            {
-                this.state.serviceCategories
-                        .map(serviceCategory =>
-                    <tr key={serviceCategory.id}>
-                <td>{serviceCategory.title}</td>
-                </tr>
-    )
-    }
-    </tbody>
-        </table>
-        </div>
-    )
+                <h3>Service Categories</h3>
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th>Category</th>
+                            <th>Popularity</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>
+                                <input
+                                    className={"form-control ml-1"}
+                                ></input>
+                            </td>
+                            <td>
+                                <input
+                                    className={"form-control ml-1"}
+                                ></input>
+                            </td>
+                            <td>
+                                <div>
+                                    <button
+                                        className="add_button btn btn-primary mx-1"><MdAdd /></button>
+                                    <button
+                                        className="add_button btn btn-success mx-1"><MdSave /></button>
+                                </div>
+                            </td>
+                        </tr>
+
+                        {
+                            this.state.serviceCategories
+                                .map(serviceCategory =>
+                                    <tr key={serviceCategory.id}>
+                                        <td>{serviceCategory.serviceCategoryName}</td>
+                                        <td>{serviceCategory.popularity}</td>
+                                        <td>
+                                            <button
+                                                className="add_button btn btn-warning mx-1"><MdEdit/></button>
+                                            <button
+                                                className="add_button btn btn-danger mx-1"><MdDelete/></button>
+                                        </td>
+                                    </tr>
+                                )
+                        }
+                    </tbody>
+                </table>
+            </div>
+        )
     }
 }
 
