b6e6b50f20a2cc5835a2eb5bdf6a390974414dc9
Michael Goodnow <mmgoodnow@gmail.com>
2019-04-22 04:24:03 -0400

    Refactor App.js to use composition
    App.js was getting riddled with stuff that should have been in
    components so I extracted these parts to GlobalNavbar.js and Profile.js.
    There was an extra Log in/Sign up in Providers page for some reason as
    well, so I deleted that.

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,8 +1,46 @@
-import React from 'react'
-import UserService from '../services/UserService'
-import Popup from "reactjs-popup";
+import React from "react";
+import {SelectUSState} from "react-select-us-states";
 
 class Profile extends React.Component {
-    
+  render() {
+    return <div className="box-layout_box">
+      <h2 ref={subtitle => this.subtitle = subtitle}>Profile</h2>
+      <hr></hr>
+      <h2>Legal Name</h2>
+      <hr></hr>
+      <form>
+        <h6>First Name</h6>
+        <input/>
+        <h6>Last Name</h6>
+        <input/>
+      </form>
+      <hr></hr>
+      <h3>Date of Birth</h3>
+      <hr></hr>
+      <form>
+        <h6>Choose your Birthday</h6>
+        <input type="month"/>
+      </form>
+      <hr></hr>
+      <h3> Home Address </h3>
+      <hr></hr>
+      <form>
+        <h6>Street</h6>
+        <input type="text"/>
+        <h6>City</h6>
+        <input type="text"/>
+        <p>
+          Select a state: <SelectUSState onChange={this.setNewValue}/>
+        </p>
+        <h6>Zip</h6>
+        <input type="text"/>
+        <h6>Email</h6>
+        <input type="text" placeholder="user@example.com"/>
+        <br/>
+        <hr></hr>
+      </form>
+    </div>
   }
+}
+
 export default Profile
