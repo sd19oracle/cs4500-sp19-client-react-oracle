41fd07add4497222e9119c1d9e4eb608b39f377a
Michael Goodnow <mmgoodnow@users.noreply.github.com>
2019-04-23 16:36:19 -0400

    Refactor FAQ Answers component as stateless

--- src/components/FAQAnswers.js
+++ src/components/FAQAnswers.js
@@ -1,56 +1,33 @@
-import React from 'react'
-import FAQAnswerService from '../services/FAQAnswerService'
 import {Link} from "react-router-dom";
+import React from "react";
 
-export default class FAQAnswers extends React.Component {
-  constructor(props) {
-    super(props);
-    this.faqAnswerService = FAQAnswerService.getInstance();
-    this.state = {
-      faqAnswers: []
-    }
-  }
-
-  componentDidMount() {
-    this.faqAnswerService
-      .findAllFAQAnswers()
-      .then(faqAnswers =>
-        this.setState({
-          faqAnswers: faqAnswers
-        })
-      )
-  }
-
-  render() {
-    return (
-      <div>
-        <h3>FAQ Answers</h3>
-        <table className="table">
-          <thead>
-          <tr>
-            <td>ID</td>
-            <td>Question ID</td>
-            <td>Question</td>
-            <td>Answer</td>
+const FAQAnswers = ({faqAnswers}) => <div>
+  <h3>FAQ Answers</h3>
+  <table className="table">
+    <thead>
+    <tr>
+      <td>ID</td>
+      <td>Question ID</td>
+      <td>Question</td>
+      <td>Answer</td>
+    </tr>
+    </thead>
+    <tbody>
+    {
+      faqAnswers
+        .map(faqAnswer =>
+          <tr key={faqAnswer.id}>
+            <td>
+              <Link to={`/admin/faq-answers/${faqAnswer.id}`}>{faqAnswer.id}</Link>
+            </td>
+            <td>{faqAnswer.questionId}</td>
+            <td>{faqAnswer.question}</td>
+            <td>{faqAnswer.answer}</td>
           </tr>
-          </thead>
-          <tbody>
-          {
-            this.state.faqAnswers
-              .map(faqAnswer =>
-                <tr key={faqAnswer.id}>
-                  <td>
-                    <Link to={`/admin/faq-answers/${faqAnswer.id}`}>{faqAnswer.id}</Link>
-                  </td>
-                  <td>{faqAnswer.questionId}</td>
-                  <td>{faqAnswer.question}</td>
-                  <td>{faqAnswer.answer}</td>
-                </tr>
-              )
-          }
-          </tbody>
-        </table>
-      </div>
-    )
-  }
-}
+        )
+    }
+    </tbody>
+  </table>
+</div>;
+
+export default FAQAnswers;
