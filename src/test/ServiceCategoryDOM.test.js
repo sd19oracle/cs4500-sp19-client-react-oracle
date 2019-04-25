319be4506376c9eaa5c359e220f2b9a56affb73b
Nianmin Guo <zenmai0822@live.com>
2019-04-25 00:05:27 -0400

    Removed Console logs and changed mocked data name

--- src/test/ServiceCategoryDOM.test.js
+++ src/test/ServiceCategoryDOM.test.js
@@ -1,6 +1,6 @@
 import "../services/ServiceCategoryService.mock"
 import { MemoryRouter } from "react-router-dom";
-import svccatmockdata from "../data/service-categories-page0.mock"
+import svccatmockdataZero from "../data/service-categories-page0.mock"
 import React from 'react'
 import renderer from 'react-test-renderer'
 import ServiceCategoryService from "../services/ServiceCategoryService"
@@ -11,13 +11,22 @@ test("render correct # of entries (first page)", () => {
     const component = renderer.create(
         <MemoryRouter>
             <ServiceCategoryTable
-                serviceCategories={svccatmockdata.content}
+                serviceCategories={svccatmockdataZero.content}
                 new_entry={{ serviceCategoryName: "", popularity: "", icon: "", id: -1 }}/>
         </MemoryRouter>
     );
     let root = component.root;
-    console.log(root.findAllByType("tr"))
     expect(root.findAllByType("tr").length).toBe(9);
 })
 
-//test("count buttons of the DOM, should be ")
+test("count buttons of the DOM, should be 7 lines + input line, 2 per line", () => { 
+    const component = renderer.create(
+        <MemoryRouter>
+            <ServiceCategoryTable
+                serviceCategories={svccatmockdataZero.content}
+                new_entry={{ serviceCategoryName: "", popularity: "", icon: "", id: -1 }}/>
+        </MemoryRouter>
+    );
+    let root = component.root;
+    expect(root.findAllByType("button").length).toBe((7+1)*2); 
+})
